ead4389ce87a4c4b60da3282a7f971ca24864a6c
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-03-05 21:52:36 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <nav>
+          <img src="src/react-logo.png" alt="React logo" />
+          <span>ReactFacts</span>
+        </nav>
+      </header>
+    </>
+  );
+}
